8b87b6eb702b948fbc55f19d91de4b4ecb73f652
Eunhyung Choi <milkiss0414@naver.com>
2022-07-22 22:22:56 +0900

    Feat: 초기환경

---
+++ Plantity/Plantity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plantity
+//
+//  Created by 최은형 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
