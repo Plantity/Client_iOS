9c7c43ceb325cb6500f1ad37d7894bb63b7fd4c0
HaileyKim <rlaekdus17@sookmyung.ac.kr>
2022-07-25 18:33:54 +0900

    Feat: 식물로그

---
+++ Plantity/Plantity/LogPage/LogPageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogPageViewController.swift
+//  Plantity
+//
+//  Created by 김다연 on 2022/07/25.
+//
+
+import UIKit
+
+class LogPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
