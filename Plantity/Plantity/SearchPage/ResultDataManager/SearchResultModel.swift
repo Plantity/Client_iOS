52775c319977f742c1074289108501988d56d380
Eunhyung Choi <milkiss0414@naver.com>
2022-07-25 15:48:55 +0900

    Update: 검색 데이터 모델

---
+++ Plantity/Plantity/SearchPage/ResultDataManager/SearchResultModel.swift
@@ -0,0 +1,16 @@
+//
+//  ResultDataModel.swift
+//  Plantity
+//
+//  Created by 최은형 on 2022/07/25.
+//
+
+import Foundation
+
+struct SearchResultModel {
+    let imageUrl: String?
+    let name: String?
+    let level: Int?
+    let Intro: String?
+    let Tag: String?
+}
