ff3ba347c05f5fc17d10725c3d1e5bcd5ca69961
Eunhyung Choi <milkiss0414@naver.com>
2022-07-26 16:27:36 +0900

    Update: Tab Bar Item UI 수정

---
+++ Plantity/Plantity/MyPage/MyPageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MyPageViewController.swift
+//  Plantity
+//
+//  Created by 최은형 on 2022/07/26.
+//
+import UIKit
+
+class MyPageViewController: UIViewController {
+    @IBOutlet weak var myCardView: UIView!
+
+    @IBOutlet weak var myProgressView: UIProgressView!
+
+    @IBOutlet weak var myImageView: UIImageView!
+
+    @IBOutlet weak var myNameLabel: UILabel!
+
+    @IBOutlet weak var myMedalImageView: UIImageView!
+
+    @IBOutlet weak var myLevelLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupAttribute()
+    }
+
+    private func setupAttribute() {
+
+    }
+}
