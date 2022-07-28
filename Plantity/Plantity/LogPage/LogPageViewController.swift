f83ef893c2d198faef640e591daccb1e4b5d3b52
HaileyKim <rlaekdus17@sookmyung.ac.kr>
2022-07-28 22:07:33 +0900

    Feat:Logpage 달력완성
    - 정해야할것
    달력 라이브러리에서 날짜에 따른 뷰 기능이 제공 안됨
    > view로 label만 날짜에 따라 바뀌게 설정
    > tableview 로 넘길수 있도록

--- Plantity/Plantity/LogPage/LogPageViewController.swift
+++ Plantity/Plantity/LogPage/LogPageViewController.swift
@@ -8,14 +8,20 @@
 import UIKit
 import FSCalendar
 
+//데이트포멧터 선언
+let dateFormatter = DateFormatter()
 
-
-class LogPageViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource {
+class LogPageViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource,FSCalendarDelegateAppearance {
     
     @IBOutlet weak var collectionView: UICollectionView!
 
+    @IBOutlet weak var messageView: UIView!
     
     @IBOutlet weak var calendarView: FSCalendar!
+    
+//    var selectedDate:Date=Date()
+    
+
     
 
     override func viewDidLoad() {
@@ -26,19 +32,59 @@ class LogPageViewController: UIViewController, FSCalendarDelegate, FSCalendarDat
         collectionView.dataSource = self
         
         
+        
+        
+        //메세지
+        messageView.clipsToBounds = true
+        messageView.layer.cornerRadius = 30
+        messageView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner,.layerMaxXMinYCorner)
+        
+        
+        //달력
+        dateFormatter.dateFormat = "yyyy-MM-dd"
         calendarView.delegate = self
         calendarView.dataSource = self
+        
+        
         //주간달력으로 변경
         calendarView.scope = .week
         //언어변경
-        calendarView.locale = Locale(identifier: "ko_KR")
+        calendarView.locale = Locale(identifier: "en_US")
+        //글자색 변경-월.연도 글씨
+        calendarView.appearance.headerTitleColor = UIColor.darkGray
+        //글자색 변경-월화수목금
+        calendarView.appearance.weekdayTextColor = UIColor.darkGray
+        //오늘해당하는 동그라미
+        calendarView.appearance.todayColor=UIColor.systemGreen
+        //날자 클릭시 동그라미
+        calendarView.appearance.selectionColor=UIColor.lightGray
 
 
         // Do any additional setup after loading the view.
+
+        
     }
     
 
+
 }
+
+
+extension ViewController:FSCalendarDelegate,FSCalendarDataSource,FSCalendarDelegateAppearance{
+    
+    // 날짜 선택 시 할일을 지정
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(dateFormatter.string(from: date) + " 선택됨")
+
+        
+    }
+    // 날짜 선택 해제 시 콜백 메소드
+    public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(dateFormatter.string(from: date) + " 해제됨")
+    }
+}
+
+
 
 class CustomFlowLayout:UICollectionViewFlowLayout{
     private var isInit: Bool = false
