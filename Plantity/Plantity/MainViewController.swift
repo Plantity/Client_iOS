00e1d9bfac42b397493456526a371694f9d75092
Eunhyung Choi <milkiss0414@naver.com>
2022-08-08 14:11:17 +0900

    Update: Tab bar 수정
    - 순서 조정
    - 처음 로드되는 화면 main으로 변경

---
+++ Plantity/Plantity/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Plantity
+//
+//  Created by 최은형 on 2022/08/08.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.selectedIndex = 2
+
+    }
+
+}
