b48f2d508865c5cb39a2d7c5d0b324bafc7d7c70
HaileyKim <rlaekdus17@sookmyung.ac.kr>
2022-08-18 19:08:41 +0900

    Fix:식물로그
    로그페이지 달력/카드 xib 만들기
    달력도 collectionview로 바꿈

--- Plantity/Plantity/LogPage/LogPageViewController.swift
+++ Plantity/Plantity/LogPage/LogPageViewController.swift
@@ -14,36 +14,14 @@ let formatter = DateFormatter()
 
 
 
-
-var CardData:[LogDataCardModel]=[]
-var arrayCard:[LogDataCardModel]=[LogDataCardModel(plantName: "치치", plantType: "몬스테라", cardMemo: "물은 하루에 한번 주세요!")]
-
-var CalendarData:[LogDataCalendar]=[]
-var arrayCalendar:[LogDataCalendar]=[LogDataCalendar(watering: true, watching: false, showering: false, soiling: true)]
-
-
 class LogPageViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource,FSCalendarDelegateAppearance {
-
     
-    //collectionView
-    @IBOutlet weak var collectionView: UICollectionView!
-
-    //calendar 요소
-    @IBOutlet weak var messageView: UIView!
     
-    @IBOutlet weak var calendarView: FSCalendar!
+    //상단카드collectionView
+    @IBOutlet weak var cardcollectionView: UICollectionView!
     
-    @IBOutlet weak var dateMessageView: UIView!
-    
-    @IBOutlet weak var wateringyesLabel: UILabel!
-    
-    @IBOutlet weak var lookingyesLabel: UILabel!
-    
-    @IBOutlet weak var showeringyesLabel: UILabel!
-    
-    @IBOutlet weak var soilingyesLabel: UILabel!
-    //    var selectedDate:Date=Date()
-    
+    //하단로그collectionView
+    @IBOutlet weak var logCollectionView: UICollectionView!
     
 
     
@@ -51,167 +29,80 @@ class LogPageViewController: UIViewController, FSCalendarDelegate, FSCalendarDat
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        
-        
-        
-        //메세지(카드)
-        messageView.clipsToBounds = true
-        messageView.layer.cornerRadius = 30
-        messageView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner,.layerMaxXMinYCorner)
-        
-        
-        //달력
-        calendarView.delegate = self
-        calendarView.dataSource = self
-        //주간달력으로 변경
-        calendarView.scope = .week
-        //언어변경
-        calendarView.locale = Locale(identifier: "en_US")
-        //글자색 변경-월.연도 글씨
-        calendarView.appearance.headerTitleColor = UIColor.darkGray
-        //글자색 변경-월화수목금
-        calendarView.appearance.weekdayTextColor = UIColor.darkGray
-        //오늘해당하는 동그라미
-        calendarView.appearance.todayColor=UIColor.systemGreen
-        //날자 클릭시 동그라미
-        calendarView.appearance.selectionColor=UIColor.lightGray
-
-
-        // Do any additional setup after loading the view.
-
-        // Carousel 적용
         setupCard()
-        setUpEvents()
+        setUplogCV()
         
     }
     
     func setupCard() {
+        cardcollectionView.dataSource=self
+        cardcollectionView.delegate=self
+        
+        let cardNib=UINib(nibName: "CollectionViewCell", bundle: nil)
+        cardcollectionView.register(cardNib, forCellWithReuseIdentifier: "CollectionViewCell")
+        
+        // Carousel 적용
         let layout = UPCarouselFlowLayout()
-        layout.itemSize = CGSize(width: collectionView.frame.width-50*2, height: 170)
+        layout.itemSize = CGSize(width: cardcollectionView.frame.width-50*2, height: 170)
         layout.scrollDirection = .horizontal
-        
         layout.sideItemAlpha = 0.3
-        
         layout.spacingMode = UPCarouselFlowLayoutSpacingMode.fixed(spacing: 10)
-        
-        collectionView.collectionViewLayout = layout
+        cardcollectionView.collectionViewLayout = layout
     }
-    
 
     
-    //이벤트 배열 선언-예시
-    var events=[Date]()
-    //실제
-    var wateringdays=[Date]()
-    var lookingdays=[Date]()
-    var showeringdays=[Date]()
-    var soilingdays=[Date]()
+
 
     
-    func setUpEvents() {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "ko_KR")
-        formatter.dateFormat = "yyyy-MM-dd"
-        let xmas = formatter.date(from: "2022-08-02")
-        let sampledate = formatter.date(from: "2022-08-01")
+    func setUplogCV(){
+        logCollectionView.dataSource=self
+        logCollectionView.delegate=self
         
-        events = [xmas!, sampledate!]
-        wateringdays = [xmas!, sampledate!]
-        lookingdays = [xmas!, sampledate!]
-        showeringdays = [xmas!, sampledate!]
-        soilingdays = [xmas!, sampledate!]
-    }
-    
-    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
-        if self.wateringdays.contains(date){
-            return 1
-        }else{
-            return 0
-        }
-    }
-    
-    // 날짜 선택 시 할일을 지정
-    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        formatter.dateFormat="yyyy-MM-dd"
-        print(formatter.string(from: date) + " 선택됨")
-        //물주기
-        if(self.events.contains(date)){
-            wateringyesLabel.text="물 주기 완료!"
-        }else{
-            wateringyesLabel.text="물을 주지 않았어요"
-        }
-        //살펴보기
-        if(self.events.contains(date)){
-            lookingyesLabel.text="살펴보기 완료!"
-        }else{
-            lookingyesLabel.text="살펴보지 않았어요"
-        }
-        //씻겨주기
-        if(self.events.contains(date)){
-            showeringyesLabel.text="씻겨주기 완료!"
-        }else{
-            showeringyesLabel.text="씻겨 주지 않았어요"
-        }
-        //분갈이
-        if(self.events.contains(date)){
-            soilingyesLabel.text="분갈이 완료!"
-        }else{
-            soilingyesLabel.text="분갈이하지 않았어요"
-        }
+        let logNib=UINib(nibName: "CalendarCollectionViewCell", bundle: nil)
+        logCollectionView.register(logNib, forCellWithReuseIdentifier: "CalendarCollectionViewCell")
+        
+        // Carousel 적용-> 형식 변경하기
+        let layout = UPCarouselFlowLayout()
+        layout.itemSize = CGSize(width: logCollectionView.frame.width, height: 500)
+        layout.scrollDirection = .horizontal
+        layout.sideItemAlpha = 0.3
+        logCollectionView.collectionViewLayout = layout
+        
         
     }
     
+
     
-    
-    // 날짜 선택 해제 시 콜백 메소드
-    public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        formatter.dateFormat="yyyy-MM-dd"
-        print(formatter.string(from: date) + " 해제됨")
-    }
 
-
-}
-
-//달력
-extension ViewController:FSCalendarDelegate,FSCalendarDataSource,FSCalendarDelegateAppearance{
-
-
-
-    // 날짜 선택 시 할일을 지정
-    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        print(formatter.string(from: date) + " 선택됨")
-
-
-    }
-    // 날짜 선택 해제 시 콜백 메소드
-    public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        print(formatter.string(from: date) + " 해제됨")
-    }
 }
 
 
 
 //카드
-extension LogPageViewController : UICollectionViewDataSource, UICollectionViewDelegate{
+extension LogPageViewController : UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
 
     //collection view의 cell 을 10개 생성
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        return 10
     }
 
     //customCell생성
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-        guard let customCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
-
-        customCell.layer.cornerRadius=10
         
+        if collectionView == cardcollectionView {
+            guard let customCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
 
+            customCell.layer.cornerRadius=10
+            return customCell
 
+        }else{
+            guard let customCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalendarCollectionViewCell", for: indexPath) as? CalendarCollectionViewCell else { return UICollectionViewCell() }
 
-                return customCell
+            customCell.layer.cornerRadius=10
+            return customCell
+        }
+
     }
 }
+
+
