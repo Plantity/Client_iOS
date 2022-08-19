cd60e1eaa4e7bdd41d8f8fd08a722710e132cb36
HaileyKim <rlaekdus17@sookmyung.ac.kr>
2022-08-19 20:52:44 +0900

    feat:메인페이지
    img->button
    버튼 누르면 모달 띄우기

--- Plantity/Plantity/MainPage/Cell/AppendCollectionViewCell.swift
+++ Plantity/Plantity/MainPage/Cell/AppendCollectionViewCell.swift
@@ -9,14 +9,33 @@ import UIKit
 
 class AppendCollectionViewCell: UICollectionViewCell {
 
-    @IBOutlet weak var cardAppend: UIImageView!
+
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        
     }
     func setupcard() {
-        cardAppend.layer.cornerRadius = 10
-        cardAppend.clipsToBounds = true
-    }
 
+    }
+    
+    @IBAction func cardAppendButton(_ sender: Any) {
+        
+//        버튼누르면 추가페이지 모달 띄우기
+        
+        let storyboard=UIStoryboard(name: "MainPageViewController", bundle: nil)
+        
+        guard let viewController = storyboard.instantiateViewController(identifier: "AddPlantViewController") as? AddPlantViewController else { return }
+
+        viewController.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+
+        
+//        self.present(viewController, animated: true, completion: nil)
+//        self.navigationController?.pushViewController(viewController, animated: true)
+        
+        
+    }
+    
 }
--- Plantity/Plantity/MainPage/MainPageViewController.swift
+++ Plantity/Plantity/MainPage/MainPageViewController.swift
@@ -123,6 +123,7 @@ extension MainPageViewController: UICollectionViewDelegate,UICollectionViewDataS
  
 
     }
+    
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 300, height: 450)
